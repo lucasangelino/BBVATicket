8d0fc37d2e5d3a20526a3bc22d418cd75a42fb87
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-01-05 17:43:43 -0300

    TCK add new ticket page

--- frontend/src/components/ticket/NewTicket.js
+++ frontend/src/components/ticket/NewTicket.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { Button, Col, Divider, Row, Typography, Card } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
+const { Title, Text } = Typography;
 
 export default function NewTicket() {
-  return <div>NewTicket</div>;
+  const createTicket = () => {};
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={1}>Generate new Ticket</Title>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={createTicket}
+          >
+            New Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={1}>Your Ticket Number</Text>
+          <br />
+          <Text level={2} type="success" style={{ fontSize: 65 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
 }
