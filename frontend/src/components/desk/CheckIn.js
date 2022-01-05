7343feef287674df8477739f5d96324cbdf915d2
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-01-05 17:24:25 -0300

    TCK add checking page

--- frontend/src/components/desk/CheckIn.js
+++ frontend/src/components/desk/CheckIn.js
@@ -1,5 +1,59 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { SaveOutlined } from "@ant-design/icons";
+import { Form, Input, Button, InputNumber, Typography, Divider } from "antd";
+
+const { Title, Text } = Typography;
 
 export default function CheckIn() {
-  return <div>CheckIn</div>;
+  const history = useNavigate();
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+    history("/desk");
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <Title level={3}>Check In</Title>
+      <Text>Enter you Name and Desk number</Text>
+      <Divider />
+
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 14 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Agente"
+          name="agente"
+          rules={[{ required: true, message: "Please input your username" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Desk"
+          name="desk"
+          rules={[{ required: true, message: "Please input your desk number" }]}
+        >
+          <InputNumber />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 14 }}>
+          <Button type="primary" htmlType="submit" shape="round">
+            <SaveOutlined />
+            Check in
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 }
--- frontend/src/components/desk/Desk.js
+++ frontend/src/components/desk/Desk.js
@@ -1,5 +1,51 @@
 import React from "react";
+import { Button, Col, Divider, Row, Typography, Card } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
+const { Title, Text } = Typography;
 
 export default function Desk() {
-  return <div>Desk</div>;
+  const closeDesk = () => {};
+
+  return (
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>Lucas</Title>
+          <Text>On desk number </Text>
+          <Text type="succes">3</Text>
+        </Col>
+        <Col span={4} align="right">
+          <Button type="danger" shape="round" onClick={closeDesk}>
+            <CloseCircleOutlined />
+            Exit
+          </Button>
+        </Col>
+      </Row>
+      <Divider />
+      <Row>
+        <Col>
+          <div className="site-card-border-less-wrapper">
+            <Card
+              title="Taking ticket number"
+              bordered={false}
+              style={{
+                width: 300,
+                textAlign: "center",
+                backgroundColor: "#009EE5",
+              }}
+            >
+              <Text style={{ fontSize: 60 }}>55</Text>
+            </Card>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button type="primary" shape="round">
+            Next Ticket
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
 }
